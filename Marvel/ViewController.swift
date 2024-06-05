c98ff0f84c915b34cc3d55f89e05a1416f7f1de1
yernar <yernar.baiginzheyev@gmail.com>
2024-06-05 21:19:42 +0500

    Initial Commit

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Yernar Baiginzheyev on 05.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
